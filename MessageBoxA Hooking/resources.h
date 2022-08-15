609f487bb4beb7441a6bdff915551000615fb36d
Sc4r3Cr0w <52085661+UncleJ4ck@users.noreply.github.com>
2022-08-15 03:05:48 +0200

    fix: "changing the header file"

--- MessageBoxA Hooking/resources.h
+++ MessageBoxA Hooking/resources.h
@@ -1,43 +1,54 @@
+#pragma once 
+
 #include <Windows.h>
-
-char MessageBoxAfirstBytes[6];
-FARPROC MessageBoxAddress;
-HANDLE Process = GetCurrentProcess();
+#include <iostream>
 
 
-int __stdcall JumpFuncAfterHook(HWND hWnd, LPCSTR lpText, LPCSTR lpCaption, UINT uType) {
+class Hooking {
+    private:
+        char MessageBoxAfirstBytes[6];
+        FARPROC MessageBoxAddress;
+        HANDLE Process = GetCurrentProcess();
+    public:
+        int __stdcall JumpFuncAfterHook(HWND hWnd, LPCSTR lpText, LPCSTR lpCaption, UINT uType) {
 
-	// ReWrite The First Bytes
-	WriteProcessMemory(Process, (LPVOID)MessageBoxAddress, MessageBoxAfirstBytes, sizeof(MessageBoxAfirstBytes), 0);
+            // ReWrite The First Bytes
+            WriteProcessMemory(Process, (LPVOID)MessageBoxAddress, MessageBoxAfirstBytes, sizeof(MessageBoxAfirstBytes), 0);
 
-	return MessageBoxA(NULL, "Hooked", "From Z0rch3r", MB_OK);
-}
+            return MessageBoxA(NULL, "Hooked", "From Z0rch3r", MB_OK);
+        }
+        void PutTheHook() {
+            // Getting The Memory Address Of MessageBoxA()
+            HINSTANCE lib = LoadLibraryA("user32.dll");
+            MessageBoxAddress = GetProcAddress(lib, "MessageBoxA");
 
-void PutTheHook() {
+            // Saving The First 6 Bytes  Of MessageBoxAfirstBytes
+            BOOL SaveOrigBytes = ReadProcessMemory(Process, MessageBoxAddress, MessageBoxAfirstBytes, 6, 0);
 
-	// Getting The Memory Address Of MessageBoxA()
-	HINSTANCE lib = LoadLibraryA("user32.dll");
-	MessageBoxAddress = GetProcAddress(lib, "MessageBoxA");
+            // Address Of JumpFuncAfterHook 
+            void* JumpFuncAfterHookAddress = &JumpFuncAfterHook;
 
-	// Saving The First 6 Bytes  Of MessageBoxAfirstBytes
-	BOOL SaveOrigBytes = ReadProcessMemory(Process, MessageBoxAddress, MessageBoxAfirstBytes, 6, 0);
+            // x68 = push
+            // xC3 = ret
 
-	// Address Of JumpFuncAfterHook 
-	void* JumpFuncAfterHookAddress = &JumpFuncAfterHook;
+            // push JumpFuncAfterHookAddress
+            // ret
 
-	// x68 = push
-	// xC3 = ret
+            char patch[6] = { 0 };
+            memcpy_s(patch, 1, "\x68", 1);
 
-	// push JumpFuncAfterHookAddress
-	// ret
+            memcpy_s(patch + 1, 4, &JumpFuncAfterHookAddress, 4);
 
-	char patch[6] = { 0 };
-	memcpy_s(patch, 1, "\x68", 1);
+            memcpy_s(patch + 5, 1, "\xC3", 1);
 
-	memcpy_s(patch + 1, 4, &JumpFuncAfterHookAddress, 4);
+            // Patching The Function
+            WriteProcessMemory(GetCurrentProcess(), (LPVOID)MessageBoxAddress, patch, sizeof(patch), 0);
+        }
+};
 
-	memcpy_s(patch + 5, 1, "\xC3", 1);
 
-	// Patching The Function
-	WriteProcessMemory(GetCurrentProcess(), (LPVOID)MessageBoxAddress, patch, sizeof(patch), 0);
-}
+
+
+
+
+
